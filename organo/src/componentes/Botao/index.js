a1e2c9a5f3b0d7865339e41b6bd28d840e463ff7
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-09-08 17:43:38 -0300

    Edição do prop do componente Botao

--- organo/src/componentes/Botao/index.js
+++ organo/src/componentes/Botao/index.js
@@ -1,7 +1,7 @@
 import './Botao.css'
 
 const Botao = props => {
-  return <button className='botao'>{props.nome}</button>;
+  return <button className='botao'>{props.children}</button>;
 };
 
 export default Botao;
--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -21,7 +21,7 @@ const Formulario = () => {
         <CampoTexto label='Cargo' placeholder='Digite o seu cargo' />
         <CampoTexto label='Imagem' placeholder='Informe o endereço da imagem' />
         <ListaSuspensa label="Time" itens={times} />
-        <Botao nome="Criar card" />
+        <Botao>Criar card</Botao>
       </form>
     </section>
   );
