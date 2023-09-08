d4a14153ee008a99ca3808833e2d9de977f82ce3
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-09-08 15:47:29 -0300

    Adição do JS do componente Campo Texto

---
+++ organo/src/componentes/CampoTexto/index.js
@@ -0,0 +1,10 @@
+const CampoTexto = () => {
+  return(
+    <div className="campo_texto">
+      <label className="campo_texto--label">Nome</label>
+      <input className="campo_texto--input" placeholder="Digite o seu nome" />
+    </div>
+  )
+}
+
+export default CampoTexto
