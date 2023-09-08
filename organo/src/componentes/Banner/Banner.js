fb921e6fa0d558ccf58da84e4266eab6b2491315
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-09-08 15:05:49 -0300

    Adição do JS do componente Banner

---
+++ organo/src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+function Banner() {
+  return(
+    <header>
+      <img src="./imagens/banner.png" alt="Banner do organo"/>
+    </header>
+  )
+}
+
+// Permite que o componente seja importado em outros arquivos
+export default Banner;
