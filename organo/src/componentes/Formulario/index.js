42f7e793c7534b0f3a3b90f63092ba756a9ee6de
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-09-08 16:49:24 -0300

    Mudança de estilização do componente Formulario

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -5,6 +5,7 @@ const Formulario = () => {
   return (
     <section className="secao">
       <form className="secao_formulario">
+        <h2 className="titulo_formulario">Preencha os dados para criar o card do colaborador:</h2>
         <CampoTexto label='Nome' placeholder='Digite o seu nome' />
         <CampoTexto label='Cargo' placeholder='Digite o seu cargo' />
         <CampoTexto label='Imagem' placeholder='Informe o endereço da imagem' />
