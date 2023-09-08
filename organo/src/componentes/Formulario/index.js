8e430104beccedae9212d6cc4e8ba85d5f5f819e
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-09-08 16:46:27 -0300

    Adição do CSS do componente Formulario

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,9 +1,10 @@
 import CampoTexto from '../CampoTexto';
+import './Formulario.css'
 
 const Formulario = () => {
   return (
-    <section>
-      <form>
+    <section className="secao">
+      <form className="secao_formulario">
         <CampoTexto label='Nome' placeholder='Digite o seu nome' />
         <CampoTexto label='Cargo' placeholder='Digite o seu cargo' />
         <CampoTexto label='Imagem' placeholder='Informe o endereço da imagem' />
