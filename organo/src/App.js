b4b01621dbcbeaa7c797dc93d7a9a4e428dbe2fe
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-09-09 15:04:10 -0300

    Adição do JS do componente Time

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
 
@@ -14,6 +15,13 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario colaboradorCadastrado={colaborador => adicionaColaborador(colaborador)}/>
+      <Time nome="Programação"/>
+      <Time nome="Front-End"/>
+      <Time nome="Data Science"/>
+      <Time nome="Devops"/>
+      <Time nome="UX e Design"/>
+      <Time nome="Mobile"/>
+      <Time nome="Inovação e Gestão"/>
     </div>
   );
 }
---
+++ organo/src/componentes/Time/index.js
@@ -0,0 +1,9 @@
+const Time = (props) => {
+  return (
+    <section>
+      <h3>{props.nome}</h3>
+    </section>
+  )
+}
+
+export default Time
