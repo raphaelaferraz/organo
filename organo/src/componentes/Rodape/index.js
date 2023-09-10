8343d20b792e164c4cba51915b4e74a0e959ee6b
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-09-10 14:38:54 -0300

    Adição do JS do componente Rodape

---
+++ organo/src/componentes/Rodape/index.js
@@ -0,0 +1,33 @@
+const Rodape = () => {
+  return (
+    <footer className="rodape">
+      <section>
+        <ul>
+          <li>
+            <a href="">
+              <img />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img />
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <img />
+            </a>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <img />
+      </section>
+      <section>
+        <p>Desenvolvido por Alura.</p>
+      </section>
+    </footer>
+  )
+}
+
+export default Rodape
