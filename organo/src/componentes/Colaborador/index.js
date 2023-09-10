53a0b8df10689b0fd61edcc644483e4f356fb24e
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-09-10 13:48:45 -0300

    Adição do CSS do componente Colaborador

--- organo/src/componentes/Colaborador/index.js
+++ organo/src/componentes/Colaborador/index.js
@@ -1,10 +1,12 @@
+import './Colaborador.css'
+
 const Colaborador = () => {
   return (
-    <div>
-      <div>
+    <div className="div-colaborador">
+      <div className="div-colaborador_cabecalho">
         <img src="https://github.com/raphaelaferraz.png" alt="Foto de perfil de Raphaela"/>
       </div>
-      <div>
+      <div className="div-colaborador_rodape">
         <h4>Raphaela Ferraz</h4>
         <h5>Front-end</h5>
       </div>
