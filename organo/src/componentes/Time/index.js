2b2351188856c869d6b265c0c25105cf4af66f92
Raphaela Ferraz <raphaelagferraz2@gmail.com>
2023-09-10 14:26:45 -0300

    Adição de condicional para exibição dos componentes de Time e Colaborador

--- organo/src/componentes/Time/index.js
+++ organo/src/componentes/Time/index.js
@@ -5,7 +5,7 @@ const Time = (props) => {
   const css = {backgroundColor: props.corSecundaria}
   
   return (
-    <section className='secao-time' style={css}>
+    props.colaboradores.length > 0 && <section className='secao-time' style={css}>
       <h3 style={{borderColor: props.corPrimaria}}className='secao-time_titulo'>{props.nome}</h3>
       <div className='colaboradores'>
         {props.colaboradores.map(colaborador => <Colaborador corDeFundo={props.corPrimaria} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
